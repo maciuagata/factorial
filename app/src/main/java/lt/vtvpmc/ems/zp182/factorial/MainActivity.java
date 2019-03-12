d6847002ba5576fa0584d5543fcfe67e1c3a1092
Agata Maciulevic <maciulewiczagata@gmail.com>
2019-03-12 21:10:34 +0200

    Initial commit

--- app/src/main/java/lt/vtvpmc/ems/zp182/factorial/MainActivity.java
+++ app/src/main/java/lt/vtvpmc/ems/zp182/factorial/MainActivity.java
@@ -1,5 +1,6 @@
 package lt.vtvpmc.ems.zp182.factorial;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,10 +16,12 @@ public class MainActivity extends AppCompatActivity {
     private EditText inputNumberField;
     private Button buttonResult;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         infoResult = (TextView) findViewById(R.id.infoResult);
         inputNumberField = (EditText) findViewById(R.id.inputNumberField);
         buttonResult = (Button) findViewById(R.id.buttonResult);
@@ -27,9 +30,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 BigInteger number = new BigInteger(inputNumberField.getText().toString());
-                BigInteger factorial = factorial(number);
-                String result = String.format("Factorial of %d is equal to %d", number,factorial);
+                BigInteger factorial = calcFactorial(number);
+                String result = String.format("Factorial of %d is equal to %d", number, factorial);
                 infoResult.setText(result);
+
             }
         });
 
@@ -38,11 +42,22 @@ public class MainActivity extends AppCompatActivity {
     private BigInteger factorial(BigInteger number) {
         if (number.compareTo(BigInteger.ONE) == -1) {
             return BigInteger.ONE;
-        }else {
+        } else {
             return number.multiply(factorial(number.subtract(BigInteger.ONE)));
-            if (number.compareTo(BigInteger.ONE) < 20) {
-                return BigInteger;
-            }
         }
+    }
+
+    private BigInteger calcFactorial(BigInteger number) {
+        BigInteger sum = BigInteger.ONE;
+//        for (BigInteger i = number; i.compareTo(BigInteger.ZERO) == 1; i = i.subtract(BigInteger.ONE)) {
+//            sum = sum.add(i.multiply(i.subtract(BigInteger.ONE)));
+//        }
+
+        while (number.compareTo(BigInteger.ZERO) == 1) {
+            sum = sum.add(sum.multiply(number.subtract(BigInteger.ONE)));
+            number = number.subtract(BigInteger.ONE);
+        }
+
+        return sum;
     }
 }
